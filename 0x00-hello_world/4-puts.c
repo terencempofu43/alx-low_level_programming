36f9ccdd5e1f9ee856828cd4fe6aed2bf08c079f
Terence Mpofu <terencempofu43@gmail.com>
2022-12-08 08:00:30 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# !/ bin / bash
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\" Programmimg is like building a multilingual puzzle);
+	return (0);		
+}
