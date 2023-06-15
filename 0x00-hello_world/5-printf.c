9db3dbc83f8d99d2f9ce6b14d513c856f3b1729e
TheSeptre_ <96541660+terencempofu43@users.noreply.github.com>
2023-06-15 22:23:57 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-/*main -Entry point
+/* main -Entry point
  *
- * Return always 0 (Success)
- * program prints a string
+ * Return: Always 0 (Success)
+ * 
  */
 int main(void)
 {
