0d0b3a9b80f987dec83fb715aa4607598afc6f61
Terence <terencempofu43@gmail.com>
2023-06-16 07:14:26 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-/*
- * main - prints single digit xnumbers of base 10
+
+/**
+ * main - Prints numbers between 0 to 9.
  *
- * Return: Always 0 (success)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++);
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
 	return (0);
 }
+
