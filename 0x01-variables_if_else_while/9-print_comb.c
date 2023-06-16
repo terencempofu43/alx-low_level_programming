b425167c4bb113503f25181eaa577b4cffa5ace9
Terence <terencempofu43@gmail.com>
2023-06-16 07:23:07 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - prints all possibl combinations of single digit numbers followed by a and space
+ * main - Prints numbers between 0 to 9 with commas and spaces.
  *
- * Retun: Always 0 (success)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++);
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 		if (i != 57)
 		{
-		
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
 	return (0);
-
 }
