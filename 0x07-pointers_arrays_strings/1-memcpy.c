53d8d649906e12ae7ab0369d02b855199334237e
Terence <terencempofu43@gmail.com>
2023-07-03 10:57:55 -0700

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "nain.h"
+
+/**
+ * _memcpy - copies @n bytes from memory @src
+ * to @dest
+ *
+ * @n: copy function
+ *
+ * @src: bytes form memory area
+ *
+ * return: a pointer to @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	unsigned int i;
+
+	for (i = 0, i < n, i++)
+	{
+		*(dest + i) = *(src = i);
+	}
+
+	return dest;
+}
