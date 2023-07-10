8d8faec7a8a447fa3243ff46ed7a2ac89e804b5b
Terence <terencempofu43@gmail.com>
2023-07-10 13:33:00 -0700

    spaces

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
-	array = malloc(sizeof(char) *size};
+	array = malloc(sizeof(char) * size};
 	if (size == 0 || array == NULL)
 		return (NULL);
 
