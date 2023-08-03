351383dd086c50c35178d002c1b3f05bc4847b17
Terence <terencempofu43@gmail.com>
2023-08-03 13:59:07 -0700

    sbinary fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,78 +1,29 @@
 #include "main.h"
 
 /**
- * _atoi - converts chars to integer
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
  *
- * @c: char to convert
- *
- * Return: converted integer
-*/
-unsigned int _atoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-
-/**
- * _strlen - gets the length of a string
- *
- * @str: string input
- *
- * Return: string length
-*/
-unsigned int _strlen(const char *str)
-{
-	unsigned int index = 0;
-
-	while (str[index] != '\0')
-		index++;
-	return (index);
-}
-
-/**
- * binary_to_uint - a function that converts a binary number
- *                  to an unsigned int
- *
- * @b: string that contains 0 and 1 characters
- *
- * Return: the number converted or 0
- *         if @b contains a character
- *         that is not 0 or 1 or when
- *         @b is null
-*/
+ * Return:converted number, or 0
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	int index;
-	unsigned int result = 0, base2 = 1,  num = 0;
+	int i;
+	unsigned int j;
 
-	/*if b is NULL return 0*/
-	if (b == NULL)
+	j = 0;
+	if (!b)
 		return (0);
-
-	#ifdef DEBUG /*print debug statement*/
-	printf("String is %s, and length is %u.\n", b, _strlen(b));
-	#endif
-
-	/*iterate through string*/
-	for (index = _strlen(b) - 1; index >= 0; index--)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		num = _atoi(b[index]); /*convert char to number*/
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Number is %u, index is %i and base is %u.\n\n", num, index, base2);
-		#endif
-
-		/*if number is not 0 or 1 return 0*/
-		if (num != 0 && num != 1)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		result += num * base2; /*enable debug to see it in action*/
-		base2 *= 2;
-
-		#ifdef DEBUG /*print debug statements*/
-		printf("Result is %u.\n", result);
-		#endif
 	}
-
-	return (result);
-
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
